912aef4ef50a860c1a008c5982d61eebe2faa9d9
Mohamed Ahmed <aero.mohamed@icloud.com>
2022-03-04 23:47:01 +0200

    delete RK.m and integrated into RBD Solver functino

--- Task 2/RBDSolver.m
+++ Task 2/RBDSolver.m
@@ -2,7 +2,13 @@
 % Algorithm in order to integrate 6 DOF set of equations
 % with a give Inital Conditions ICs, for single dt time step.
 function state = RBDSolver(ICs, dt)
-    K = RK4(ICs, dt);
+    K = zeros(12, 4);
+    
+    K(:, 1) = dt*DOF6(0, ICs );
+    K(:, 2) = dt*DOF6(0, ICs+0.5*K(:, 1) );
+    K(:, 3) = dt*DOF6(0, ICs+0.5*K(:, 2) );
+    K(:, 4) = dt*DOF6(0, ICs+K(:, 3) );
+    
     state = ICs + (...
         K(:, 1)+...
         2*K(:, 2)+...
